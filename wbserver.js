1e14718f37894eaf923cd6847eddea04f5ba82cb
suyungshan <samsys2324@gmail.com>
2024-04-27 16:44:04 +0800

    嘗試讓後端先連線成功

--- wbserver.js
+++ wbserver.js
@@ -6,11 +6,6 @@ import cors from "cors";
 const app = express();
 const server = createServer(app);
 const PORT = process.env.PORT || 3001;
-const messageData = [];
-const io = new Server(server, {
-  cors: corsOptions,
-});
-
 const corsOptions = {
   origin: ["http://localhost:3000", "https://10-hit-game-backend.vercel.app/"],
   methods: ["GET", "POST"],
@@ -21,20 +16,32 @@ app.use(cors(corsOptions));
 
 app.get("/", (req, res) => {
   res.send("WebSocket server is running!");
+  console.log("6666");
 });
+
+const io = new Server(server, {
+  cors: corsOptions,
+});
+
+const messageData = [];
 
 io.on("connection", (socket) => {
   console.log("A user connected");
+
   socket.on("sendMessage", () => {
     io.emit("allMessage", "hollow");
   });
+
   socket.on("Start", () => {
     io.emit("redirectToGame");
   });
+
   socket.on("hit", (data) => {
+    // Add the received data to the messageData array
     messageData.push(data);
     io.emit("allMessage", messageData);
   });
+
   socket.on("disconnect", () => {
     console.log("User disconnected");
   });
