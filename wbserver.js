2f89009638684b72b54fab3b0fe512b3bc137495
suyungshan <samsys2324@gmail.com>
2024-04-27 16:14:52 +0800

    改回用 express 佈署

--- wbserver.js
+++ wbserver.js
@@ -1,86 +1,55 @@
-import cors from "cors";
+import express from "express";
 import { createServer } from "http";
 import { Server } from "socket.io";
+import cors from "cors";
 
+const app = express();
+const server = createServer(app);
 const corsOptions = {
   origin: [
     "http://localhost:3000",
     /^https:\/\/10-hit-game-backend\.vercel\.app\//,
   ],
   methods: ["GET", "POST"],
+  allowedHeaders: ["Content-Type", "Authorization"],
 };
 
-const corsMiddleware = cors(corsOptions);
+app.use(cors(corsOptions));
 
-const server = createServer();
-const messageData = [];
-const PORT = process.env.PORT || 3002;
+app.get("/", (req, res) => {
+  res.send("WebSocket server is running!");
+});
 
 const io = new Server(server, {
   cors: corsOptions,
 });
 
-// 先暫存請求
-const requestQueue = [];
-
-// 註冊中間件
-server.on("request", async (req, res) => {
-  requestQueue.push({ req, res });
-});
-
-server.on("listening", async () => {
-  console.log("web connect"); // 在這裡輸出 "web connect"
-
-  for (const { req, res } of requestQueue) {
-    try {
-      await corsMiddlewareWrapper(req, res);
-      // 繼續處理請求
-    } catch (err) {
-      console.error("CORS錯誤:", err);
-      res.statusCode = 500;
-      res.end();
-    }
-  }
-  requestQueue.length = 0;
-});
-
-const corsMiddlewareWrapper = (req, res) => {
-  return new Promise((resolve, reject) => {
-    corsMiddleware(req, res, (err) => {
-      if (err) {
-        reject(err);
-      } else {
-        resolve();
-      }
-    });
-  });
-};
-
-server.listen(PORT, () => {
-  console.log(`WebSocket server is listening on port ${PORT}`);
-});
-
-server.listen("https://10-hit-game-backend.vercel.app/", () => {
-  console.log(
-    `WebSocket server is listening on port https://10-hit-game-backend.vercel.app/`
-  );
-});
+const messageData = [];
 
 io.on("connection", (socket) => {
-  console.log("websocket connect"); // 在這裡輸出 "websocket connect"
   console.log("A user connected");
 
   socket.on("sendMessage", () => {
     io.emit("allMessage", "hollow");
   });
+
   socket.on("Start", () => {
     io.emit("redirectToGame");
   });
+
   socket.on("hit", (data) => {
+    // Add the received data to the messageData array
     messageData.push(data);
     io.emit("allMessage", messageData);
   });
+
   socket.on("disconnect", () => {
     console.log("User disconnected");
   });
+});
+
+const PORT = process.env.PORT || 3001;
+
+server.listen(PORT, () => {
+  console.log(`WebSocket server is listening on port ${PORT}`);
 });
