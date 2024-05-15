5ae50998334eaef1202f69676b2daf68dd2e0217
suyungshan <samsys2324@gmail.com>
2024-05-16 00:48:45 +0800

    數據傳遞邏輯完成

--- wbserver.js
+++ wbserver.js
@@ -39,9 +39,36 @@ io.on("connection", (socket) => {
     io.emit("redirectToGame");
   });
 
+  socket.on("enterName", (data) => {
+    // Check if messageData already contains an object with the same name
+    const existingIndex = messageData.findIndex(
+      (item) => item.name === data.name
+    );
+
+    if (existingIndex !== -1) {
+      // If an object with the same name exists, send a warning message back to the client
+      socket.emit("warning", "A user with this name already exists.");
+      return; // Exit the function early
+    } else {
+      // If not, add the new object to messageData
+      messageData.push(data);
+      io.emit("allName", messageData);
+    }
+  });
+
   socket.on("hit", (data) => {
-    // Add the received data to the messageData array
-    messageData.push(data);
+    // 檢查 messageData 中是否已經存在相同 name 的物件
+    const existingIndex = messageData.findIndex(
+      (item) => item.name === data.name
+    );
+
+    if (existingIndex !== -1) {
+      // 如果存在，則覆蓋該物件
+      messageData[existingIndex] = data;
+    } else {
+      // 如果不存在，則添加新的物件
+      messageData.push(data);
+    }
     io.emit("allMessage", messageData);
   });
 
